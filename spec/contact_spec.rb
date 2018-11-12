71e6ca85537032206ead64805c3f86f0182788ca
itsellej <daniellehallal@gmail.com>
2018-11-12 15:21:58 +0000

    Test: creates a new instance of the Contact class

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -1,6 +1,6 @@
 require 'contact'
 
-RSpec.describe Person do
+RSpec.describe Contact do
   it "creates a new instance of the Contact class" do
     my_contacts = Contact.new
     expect(my_contacts.is_a?Object).to eq(true)
