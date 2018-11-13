d08971d40d59daef91e336c3ac6b40f8a8cd0f8a
itsellej <daniellehallal@gmail.com>
2018-11-13 09:30:39 +0000

    Test: create_new_entry method accepts input, creates new instance of Person and adds to @contacts twice

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -11,24 +11,34 @@ RSpec.describe Contact do
     expect(my_contacts.contacts).to eq([])
   end
 
-  # xit "creates a new contact using the Person class and adds it to @contacts" do
-  #   my_contacts = Contact.new
-  #   my_contacts.create_new_entry
-  #   expect(my_contacts.contacts.empty?).to eq(false)
-  # end
-  #
-  # xit "creates two new contacts using the Person class and adds both to @contacts" do
-  #   my_contacts = Contact.new
-  #   my_contacts.create_new_entry
-  #   my_contacts.create_new_entry
-  #   expect(my_contacts.contacts.length).to eq(2)
-  # end
+  it "creates a new contact using the Person class and adds it to @contacts" do
+    allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999")
+    my_contacts = Contact.new
+    my_contacts.create_new_entry
+    expect(my_contacts.contacts.empty?).to eq(false)
+  end
+
+  it "creates two new contacts using the Person class and adds both to @contacts" do
+    allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999", "Deneice", "Daniels", "dee@hello.com", "07889999999")
+    my_contacts = Contact.new
+    my_contacts.create_new_entry
+    my_contacts.create_new_entry
+    expect(my_contacts.contacts.length).to eq(2)
+  end
 
   it "create_new_entry method accepts input, creates new instance of Person and adds to @contacts" do
     allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999")
     contacts = Contact.new
     contacts.create_new_entry
     expect(contacts.contacts).to eq([{:first_name=>"Elle", :last_name=>"H", :email_address=>"elle@hello.com", :phone_number=>"07999999999"}])
+  end
+
+  it "create_new_entry method accepts input, creates new instance of Person and adds to @contacts twice" do
+    allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999", "Deneice", "Daniels", "dee@hello.com", "07889999999")
+    my_contacts = Contact.new
+    my_contacts.create_new_entry
+    my_contacts.create_new_entry
+    expect(my_contacts.contacts).to eq([{:first_name=>"Elle", :last_name=>"H", :email_address=>"elle@hello.com", :phone_number=>"07999999999"}, {:first_name=>"Deneice", :last_name=>"Daniels", :email_address=>"dee@hello.com", :phone_number=>"07889999999"}])
   end
 
 end
