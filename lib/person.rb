cae27e8c2876105d1fd4b392d7e454e2c11ca5b3
Elle H <itsellej@gmail.com>
2018-11-13 17:23:14 +0000

    Remove redundant methods from Person class. Remove redundant tests from person_spec.rb

--- lib/person.rb
+++ lib/person.rb
@@ -9,42 +9,6 @@ class Person
     @phone_number = phone
   end
 
-  def get_display_person_details
-    get_first_name
-    get_last_name
-    get_email_address
-    get_phone_number
-    display_person
-  end
-
-  def get_first_name
-    puts "Please enter the first name below:"
-    @first_name = $stdin.gets.chomp.capitalize
-  end
-
-  def get_last_name
-    puts "Please enter the last name below:"
-    @last_name = $stdin.gets.chomp.capitalize
-  end
-
-  def get_email_address
-    puts "Please enter the email address below:"
-    @email_address = $stdin.gets.chomp.downcase
-  end
-
-  def get_phone_number
-    puts "Please enter the phone number below:"
-    @phone_number = $stdin.gets.chomp
-  end
-
-  def display_person
-    puts "Here's your contact:"
-    puts "First name: #{@first_name}"
-    puts "Last name: #{@last_name}"
-    puts "Email address: #{@email_address}"
-    puts "Phone number: #{@phone_number}"
-  end
-
   def create_person_hash
     {
       :first_name => @first_name,
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -26,34 +26,4 @@ RSpec.describe Person do
     expect(my_person.phone_number).to eq("no value")
   end
 
-  it "accepts 'Elle' as input for get_first_name and sets value of first_name to Elle" do
-    allow($stdin).to receive(:gets).and_return("Elle")
-    my_person = Person.new
-    my_person.get_first_name
-    expect(my_person.first_name).to eq("Elle")
-  end
-
-  it "accepts 'Tamara' as input for get_last_name sets value of last_name to Tamara" do
-    allow($stdin).to receive(:gets).and_return("Tamara")
-    my_person = Person.new
-    my_person.get_last_name
-    expect(my_person.last_name).to eq("Tamara")
-  end
-
-  it "accepts 'elle@elle.com' as input for get_email_address. sets value of email_address to elle@elle.com" do
-    allow($stdin).to receive(:gets).and_return("elle@elle.com")
-    my_person = Person.new
-    my_person.get_email_address
-    expect(my_person.email_address).to eq("elle@elle.com")
-  end
-
-  it "accepts '07999999999' as input for get_phone_number. sets value of phone_number to 07999999999" do
-    allow($stdin).to receive(:gets).and_return("07999999999")
-    my_person = Person.new
-    my_person.get_phone_number
-    puts my_person.phone_number
-    expect(my_person.phone_number).to eq("07999999999")
-  end
-
-
 end
