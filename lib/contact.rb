6a86993eff62c3fa4f1e54b05caad28d056b78ba
Elle H <itsellej@gmail.com>
2018-11-13 17:15:08 +0000

    Amend create_new_entry method. all tests passing

--- lib/contact.rb
+++ lib/contact.rb
@@ -9,21 +9,19 @@ class Contact
 
   def create_new_entry
     puts "Please enter the first name below:"
-    first_name = $stdin.gets.chomp.capitalize
+    first = $stdin.gets.chomp.capitalize
 
     puts "Please enter the last name below:"
-    last_name = $stdin.gets.chomp.capitalize
+    last = $stdin.gets.chomp.capitalize
 
     puts "Please enter the email address below:"
-    email_address = $stdin.gets.chomp.downcase
+    email = $stdin.gets.chomp.downcase
 
     puts "Please enter the phone number below:"
-    phone_number = $stdin.gets.chomp
+    phone = $stdin.gets.chomp
 
 
-    new_contact = Person.new(first_name, last_name, email_address, phone_number)
-    
-    new_contact.get_display_person_details
+    new_contact = Person.new(first, last, email, phone)
     @contacts.push(new_contact.create_person_hash)
   end
 
