d920b7d2fc4ca31e41873c350688eabf1b830cd9
itsellej <daniellehallal@gmail.com>
2018-11-13 11:10:14 +0000

    Amend search_last_name and search_first_name methods to puts output

--- lib/contact.rb
+++ lib/contact.rb
@@ -37,17 +37,35 @@ class Contact
     end
   end
 
+
   def search_first_name(search_term)
     sort_by_first_name
-    @contacts.select do |entry|
+    search = @contacts.select do |entry|
       entry[:first_name].start_with?(search_term)
+    end
+
+    puts "First names which start with #{search_term}"
+    puts ""
+    search.each do |entry|
+      entry.each do |key, value|
+        puts "#{key.to_s}: #{value}"
+      end
+      puts ""
     end
   end
 
   def search_last_name(search_term)
     sort_by_first_name
-    @contacts.select do |entry|
+    search = @contacts.select do |entry|
       entry[:last_name].start_with?(search_term)
+    end
+
+    puts "Last names which start with #{search_term}"
+    search.each do |entry|
+      entry.each do |key, value|
+        puts "#{key.to_s}: #{value}"
+      end
+      puts ""
     end
   end
 
