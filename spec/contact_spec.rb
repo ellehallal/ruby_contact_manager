a670577d36e5a2306c68c0f35f370be7fc536a60
itsellej <daniellehallal@gmail.com>
2018-11-14 11:08:38 +0000

    Change key in hash from symbol to string

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe Contact do
     allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999")
     contacts = Contact.new
     contacts.create_new_entry
-    expect(contacts.contacts).to eq([{:first_name=>"Elle", :last_name=>"H", :email_address=>"elle@hello.com", :phone_number=>"07999999999"}])
+    expect(contacts.contacts).to eq([{"first_name":"Elle", "last_name":"H", "email_address":"elle@hello.com", "phone_number":"07999999999"}])
   end
 
   it "create_new_entry method accepts input, creates new instance of Person and adds to @contacts twice" do
@@ -38,7 +38,7 @@ RSpec.describe Contact do
     my_contacts = Contact.new
     my_contacts.create_new_entry
     my_contacts.create_new_entry
-    expect(my_contacts.contacts).to eq([{:first_name=>"Deneice", :last_name=>"Daniels", :email_address=>"dee@hello.com", :phone_number=>"07889999999"}, {:first_name=>"Elle", :last_name=>"H", :email_address=>"elle@hello.com", :phone_number=>"07999999999"}])
+    expect(my_contacts.contacts).to eq([{"first_name":"Deneice", "last_name":"Daniels", "email_address":"dee@hello.com", "phone_number":"07889999999"}, {"first_name": "Elle", "last_name":"H", "email_address":"elle@hello.com", "phone_number":"07999999999"}])
   end
 
   it "sorts @contacts in alphabetical order by first name" do
@@ -47,7 +47,7 @@ RSpec.describe Contact do
     my_contacts.create_new_entry
     my_contacts.create_new_entry
     my_contacts.create_new_entry
-    expect(my_contacts.display_contacts).to eq([{:first_name=>"Amy", :last_name=>"Winchester", :email_address=>"amy@amy.com", :phone_number=>"07000000000"}, {:first_name=>"Deneice", :last_name=>"Daniels", :email_address=>"dee@hello.com", :phone_number=>"07889999999"}, {:first_name=>"Elle", :last_name=>"H", :email_address=>"elle@hello.com", :phone_number=>"07999999999"}])
+    expect(my_contacts.display_contacts).to eq([{"first_name":"Amy", "last_name":"Winchester", "email_address":"amy@amy.com", "phone_number":"07000000000"}, {"first_name":"Deneice","last_name":"Daniels","email_address":"dee@hello.com","phone_number":"07889999999"}, {"first_name":"Elle", "last_name":"H", "email_address":"elle@hello.com", "phone_number":"07999999999"}])
   end
 
   it "displays contacts with the first name beginning with E only, in alphabetical order" do
@@ -56,7 +56,7 @@ RSpec.describe Contact do
     my_contacts.create_new_entry
     my_contacts.create_new_entry
     my_contacts.create_new_entry
-    expect(my_contacts.search_first_name("E")).to eq([{:first_name=>"Edward", :last_name=>"Smith", :email_address=>"e@smith.com", :phone_number=>"07000000000"}, {:first_name=>"Elle", :last_name=>"Smith", :email_address=>"elle@hello.com", :phone_number=>"07999999999"}])
+    expect(my_contacts.search_first_name("E")).to eq([{"first_name":"Edward", "last_name":"Smith", "email_address":"e@smith.com", "phone_number":"07000000000"}, {"first_name":"Elle", "last_name":"Smith", "email_address":"elle@hello.com", "phone_number":"07999999999"}])
   end
 
   it "displays contacts with the first name beginning with Sm only. Results sorted by first name" do
@@ -65,7 +65,7 @@ RSpec.describe Contact do
     my_contacts.create_new_entry
     my_contacts.create_new_entry
     my_contacts.create_new_entry
-    expect(my_contacts.search_last_name("Sm")).to eq([{:first_name=>"Deneice", :last_name=>"Smith", :email_address=>"dee@hello.com", :phone_number=>"07889999999"}, {:first_name=>"Edward", :last_name=>"Smith", :email_address=>"e@smith.com", :phone_number=>"07000000000"}])
+    expect(my_contacts.search_last_name("Sm")).to eq([{"first_name":"Deneice", "last_name":"Smith", "email_address":"dee@hello.com", "phone_number":"07889999999"}, {"first_name":"Edward", "last_name":"Smith", "email_address":"e@smith.com", "phone_number":"07000000000"}])
   end
 
 end
