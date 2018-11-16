4b277f6d5ea1448a8df070d066ffae8d2b049ff4
itsellej <daniellehallal@gmail.com>
2018-11-16 09:13:13 +0000

    Add before block to contact_spec

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -1,34 +1,36 @@
 require 'contact'
 
 RSpec.describe Contact do
-  xit "creates a new instance of the Contact class" do
+  before(:each) do
+   allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999")
+  end
+
+  it "creates a new instance of the Contact class" do
     my_contacts = Contact.new
     expect(my_contacts.is_a?Object).to eq(true)
   end
 
-  xit "creates a new contact using the Person class and adds it to @contacts" do
-    allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999")
+  it "creates a new contact using the Person class and adds it to @contacts" do
     my_contacts = Contact.new
     my_contacts.create_new_entry
     expect(my_contacts.contacts.empty?).to eq(false)
   end
 
-  xit "create_new_entry method accepts input, creates new instance of Person and adds to @contacts" do
-    allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999")
+  it "create_new_entry method accepts input, creates new instance of Person and adds to @contacts" do
     contacts = Contact.new
     contacts.create_new_entry
     expect(contacts.contacts).to eq([{"first_name" => "Elle", "last_name" => "H", "email_address" => "elle@hello.com", "phone_number" => "07999999999"}])
   end
 
-  xit "create_new_entry method accepts input, creates new instance of Person and adds to @contacts twice" do
-    allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999", "Deneice", "Daniels", "dee@hello.com", "07889999999")
+  it "create_new_entry method accepts input, creates new instance of Person and adds to @contacts twice" do
+    allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999",  "Deneice", "Daniels", "dee@hello.com", "07889999999")
     my_contacts = Contact.new
     my_contacts.create_new_entry
     my_contacts.create_new_entry
     expect(my_contacts.contacts).to eq([{"first_name"=> "Deneice", "last_name"=> "Daniels", "email_address"=> "dee@hello.com", "phone_number"=> "07889999999"}, {"first_name"=>  "Elle", "last_name"=> "H", "email_address"=> "elle@hello.com", "phone_number"=> "07999999999"}])
   end
 
-  xit "sorts @contacts in alphabetical order by first name" do
+  it "sorts @contacts in alphabetical order by first name" do
     allow($stdin).to receive(:gets).and_return("Elle", "H", "elle@hello.com", "07999999999", "Deneice", "Daniels", "dee@hello.com", "07889999999", "Amy", "Winchester", "amy@amy.com", "07000000000")
     my_contacts = Contact.new
     my_contacts.create_new_entry
@@ -37,7 +39,7 @@ RSpec.describe Contact do
     expect(my_contacts.display_contacts).to eq([{"first_name"=> "Amy", "last_name"=> "Winchester", "email_address"=> "amy@amy.com", "phone_number"=> "07000000000"}, {"first_name"=> "Deneice","last_name"=> "Daniels","email_address"=> "dee@hello.com","phone_number"=> "07889999999"}, {"first_name"=> "Elle", "last_name"=> "H", "email_address"=> "elle@hello.com", "phone_number"=> "07999999999"}])
   end
 
-  xit "displays contacts with the first name beginning with E only, in alphabetical order" do
+  it "displays contacts with the first name beginning with E only, in alphabetical order" do
     allow($stdin).to receive(:gets).and_return("Elle", "Smith", "elle@hello.com", "07999999999", "Deneice", "Daniels", "dee@hello.com", "07889999999", "Edward", "Smith", "e@smith.com", "07000000000")
     my_contacts = Contact.new
     my_contacts.create_new_entry
@@ -46,7 +48,7 @@ RSpec.describe Contact do
     expect(my_contacts.search_first_name("E")).to eq([{"first_name" => "Edward", "last_name" => "Smith", "email_address" => "e@smith.com", "phone_number" => "07000000000"}, {"first_name" => "Elle", "last_name" => "Smith", "email_address" => "elle@hello.com", "phone_number" => "07999999999"}])
   end
 
-  xit "displays contacts with the first name beginning with Sm only. Results sorted by first name" do
+  it "displays contacts with the first name beginning with Sm only. Results sorted by first name" do
     allow($stdin).to receive(:gets).and_return("Elle", "Dorie", "elle@hello.com", "07999999999", "Deneice", "Smith", "dee@hello.com", "07889999999", "Edward", "Smith", "e@smith.com", "07000000000")
     my_contacts = Contact.new
     my_contacts.create_new_entry
@@ -55,7 +57,7 @@ RSpec.describe Contact do
     expect(my_contacts.search_last_name("Sm")).to eq([{"first_name" => "Deneice", "last_name" => "Smith", "email_address" => "dee@hello.com", "phone_number" => "07889999999"}, {"first_name" => "Edward", "last_name" => "Smith", "email_address" => "e@smith.com", "phone_number" => "07000000000"}])
   end
 
-  xit "displays contacts with the email address beginning with 'E' only" do
+  it "displays contacts with the email address beginning with 'E' only" do
     allow($stdin).to receive(:gets).and_return("Elle", "Dorie", "elle@hello.com", "07999999999", "Deneice", "Smith", "dee@hello.com", "07889999999", "Edward", "Smith", "e@smith.com", "07000000000")
     my_contacts = Contact.new
     my_contacts.create_new_entry
