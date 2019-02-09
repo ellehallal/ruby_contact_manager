3c9b4394dfd102c4f6222866331a895ba76516d9
Elle Hallal <daniellehallal@gmail.com>
2019-02-09 19:23:22 +0000

    Add is_phone_number_valid method. Tests passing

--- lib/validation.rb
+++ lib/validation.rb
@@ -10,4 +10,8 @@ class Validation
   def is_name_valid?(name)
     name.match?(/^[-a-zA-Z ]*$/) && name.length > 1 && name.length < 50
   end
+
+  def is_phone_number_valid?(number)
+    number.scan(/\D/).empty? && number.length.between?(7, 11)
+  end
 end
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Validation do
     check_name2 = @validation.is_name_valid?('Hannah Rose')
     check_name3 = @validation.is_name_valid?('Hannah-Rose')
     expect(check_name1).to eq(true)
-    # expect(check_name2).to eq(true)
+    expect(check_name2).to eq(true)
     expect(check_name3).to eq(true)
   end
 
@@ -31,5 +31,22 @@ RSpec.describe Validation do
     expect(check_name2).to eq(false)
     expect(check_name3).to eq(false)
   end
+
+  it 'returns true if phone number is valid' do
+    check_number1 = @validation.is_phone_number_valid?('07898987897')
+    check_number2 = @validation.is_phone_number_valid?('02070000000')
+    expect(check_number1).to eq(true)
+    expect(check_number2).to eq(true)
+  end
+
+  it 'returns false if phone number is not valid' do
+    check_number1 = @validation.is_phone_number_valid?('098u898y89y')
+    check_number2 = @validation.is_phone_number_valid?('222222')
+    check_number3 = @validation.is_phone_number_valid?('(0203)2222')
+    expect(check_number1).to eq(false)
+    expect(check_number2).to eq(false)
+    expect(check_number3).to eq(false)
+  end
+
   
 end
